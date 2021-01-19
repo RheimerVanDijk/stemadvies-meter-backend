2e3a5bd2a1aae7b681b5361269527df904bc2caf
Yoran de Quay <yoranq@hotmail.com>
2021-01-19 11:45:01 +0100

    added getParties and chosenParties method

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require_once ("src/classes/questionsClass.php");
+require_once ("src/classes/politicialPartiesClass.php");
 $questionsClass = (new questions());
+$partiesClass = (new parties());
 $result = $questionsClass->getQuestions();
-var_dump($result);
+$chosenParties = $partiesClass->chosenParties(1);
+$getParties = $partiesClass->getParties(1);
+var_dump($chosenParties);
+var_dump($getParties);
+//var_dump($result);
--- src/classes/politicialPartiesClass.php
+++ src/classes/politicialPartiesClass.php
@@ -5,10 +5,42 @@
  * Date: 19-1-2021
  * Time: 09:56
  */
-
+require_once ("dbClass.php");
 class parties
 {
     public function getParties() {
+        try {
+            $connection = (new db)->connect();
+            $stmt = $connection->prepare('SELECT * FROM `political_parties`');
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result;
+        }
+        catch (PDOException $e) {
+            return json_encode([
+                'type' => 'error',
+                'msg' => $e->getMessage()
+            ]);
+        }
+    }
 
+    public function chosenParties($partyID) {
+        try {
+            $connection = (new db)->connect();
+            $stmt = $connection->prepare('UPDATE `political_parties` SET ammount_chosen = ammount_chosen + 1 WHERE party_id = :party_id');
+            $stmt->execute([
+                "party_id" => $partyID,
+            ]);
+            return json_encode([
+                'type' => 'success',
+                'msg' => 'De kolom van de gekozen partijs is successvol verhoogd'
+            ]);
+        }
+        catch (PDOException $e) {
+            return json_encode([
+                'type' => 'error',
+                'msg' => $e->getMessage()
+            ]);
+        }
     }
 }
