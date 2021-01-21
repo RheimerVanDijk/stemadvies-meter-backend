af8ed49624043645a28221c366a2f93ec9747d80
OneshotFire <jmartijnd@gmail.com>
2021-01-21 17:38:43 +0100

    added deleteQuestions api

--- src/classes/questionsClass.php
+++ src/classes/questionsClass.php
@@ -24,11 +24,9 @@ class questions
         try {
             $connection = (new db)->connect();
             $stmt = $connection->prepare('INSERT INTO `questions` SET question = :question, axis = :axis, value = :value');
-
             $this->question = htmlspecialchars(strip_tags($this->question));
             $this->axis = htmlspecialchars(strip_tags($this->axis));
             $this->value = htmlspecialchars(strip_tags($this->value));
-
             $stmt->bindParam(':question', $this->question);
             $stmt->bindParam(':axis', $this->axis);
             $stmt->bindParam(':value', $this->value);
@@ -50,12 +48,10 @@ class questions
         try {
             $connection = (new db)->connect();
             $stmt = $connection->prepare('UPDATE `questions` SET question = :question, axis = :axis, value = :value WHERE question_id = :question_id');
-
             $this->question_id = htmlspecialchars(strip_tags($this->question_id));
             $this->question = htmlspecialchars(strip_tags($this->question));
             $this->axis = htmlspecialchars(strip_tags($this->axis));
             $this->value = htmlspecialchars(strip_tags($this->value));
-
             $stmt->bindParam(':question_id', $this->question_id);
             $stmt->bindParam(':question', $this->question);
             $stmt->bindParam(':axis', $this->axis);
@@ -64,6 +60,26 @@ class questions
             return json_encode([
                 'type' => 'success',
                 'msg' => 'Vraag is succesvol geupdatet'
+            ]);
+        } catch (PDOException $e) {
+            return json_encode([
+                'type' => 'error',
+                'msg' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function deleteQuestions()
+    {
+        try {
+            $connection = (new db)->connect();
+            $stmt = $connection->prepare('DELETE FROM `questions` WHERE question_id = :question_id');
+            $this->question_id = htmlspecialchars(strip_tags($this->question_id));
+            $stmt->bindParam(':question_id', $this->question_id);
+            $stmt->execute();
+            return json_encode([
+                'type' => 'success',
+                'msg' => 'Vraag is succesvol gedeletet'
             ]);
         } catch (PDOException $e) {
             return json_encode([
