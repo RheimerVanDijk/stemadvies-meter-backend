3b9e9a6e7e9018926c8ca3571596aba6b75f5383
OneshotFire <jmartijnd@gmail.com>
2021-01-21 16:48:58 +0100

    added updateQuestion api

--- src/classes/questionsClass.php
+++ src/classes/questionsClass.php
@@ -44,4 +44,32 @@ class questions
             ]);
         }
     }
+
+    public function updateQuestions()
+    {
+        try {
+            $connection = (new db)->connect();
+            $stmt = $connection->prepare('UPDATE `questions` SET question = :question, axis = :axis, value = :value WHERE question_id = :question_id');
+
+            $this->question_id = htmlspecialchars(strip_tags($this->question_id));
+            $this->question = htmlspecialchars(strip_tags($this->question));
+            $this->axis = htmlspecialchars(strip_tags($this->axis));
+            $this->value = htmlspecialchars(strip_tags($this->value));
+
+            $stmt->bindParam(':question_id', $this->question_id);
+            $stmt->bindParam(':question', $this->question);
+            $stmt->bindParam(':axis', $this->axis);
+            $stmt->bindParam(':value', $this->value);
+            $stmt->execute();
+            return json_encode([
+                'type' => 'success',
+                'msg' => 'Vraag is succesvol geupdatet'
+            ]);
+        } catch (PDOException $e) {
+            return json_encode([
+                'type' => 'error',
+                'msg' => $e->getMessage()
+            ]);
+        }
+    }
 }
