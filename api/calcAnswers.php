e190f5514749f34c18c0eec3f4d48760c55c7a09
Rheimer <Rheimer@icloud.com>
2021-01-25 14:50:40 +0100

    added post data to calc api

--- api/calcAnswers.php
+++ api/calcAnswers.php
@@ -7,9 +7,11 @@ header('Content-Type: application/json');
 include_once '../src/classes/answersClass.php';
 include_once '../src/classes/politicialPartiesClass.php';
 
+$data = $_POST['answersJsonArr'];
+
 $answersClass = (new answersClass());
 $politicialPartyClass= (new parties());
-$answersResult = $answersClass->calculateAnswers($answersJsonArr);
+$answersResult = $answersClass->calculateAnswers($data);
 $answersResult = $politicialPartyClass->partyResult($answersResult);
 echo json_encode($answersResult, JSON_PRETTY_PRINT);
 echo json_encode($politicialPartyClass, JSON_PRETTY_PRINT);
